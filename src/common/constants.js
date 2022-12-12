4ea22fade6dfa8dfc1abc22d7de68110871aca5e
kkdevs <kkdevs17@gmail.com>
2022-12-12 14:00:31 +0500

    done

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,4 @@
-const SERVER_BASE_URL = "https://kk-time-table-server.herokuapp.com";
+const SERVER_BASE_URL = "https://kk-it-solutions.herokuapp.com";
 // const SERVER_BASE_URL = "http://localhost:5000";
 const CLIENT_BASE_URL = "http://localhost:3000/";
 
